f812fe0d5f07bfbe0ec4c25b269c384304281210
ben_blake <benblake019@gmail.com>
2017-11-22 12:47:24 -0500

    Added all static views - half way point of ch4 in getting MEAN

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,14 @@
+/* GET home page. */
+module.exports.homeList = function(req, res, next) {
+  res.render('locations-list', { title: 'Home' });
+};
+
+/* GET location info page. */
+module.exports.locationInfo = function(req, res, next) {
+  res.render('location-info', { title: 'Location info' });
+};
+
+/* GET add review info page. */
+module.exports.addReview = function(req, res, next) {
+  res.render('location-review-form', { title: 'Add review' });
+};
