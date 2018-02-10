db644986a72c8d6efea01208580b4362939b7242
ben_blake <benblake019@gmail.com>
2018-02-10 14:47:44 -0500

    Frontend consuming REST API. Completes chapter 7 of getting MEAN book.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,5 +1,21 @@
-/* GET home page. */
-module.exports.homeList = function(req, res, next) {
+var request = require('request');
+var apiOptions = {
+  server : "http://localhost:3000"
+};
+if (process.env.NODE_ENV === 'production') {
+  apiOptions.server = "https://cryptic-sands-94763.herokuapp.com";
+}
+
+var renderHomepage = function(req, res, responseBody) {
+  var message;
+  if (!(responseBody instanceof Array)) {
+    message = "API lookup error";
+    responseBody = [];
+  } else {
+    if (!responseBody.length) {
+      message = "No places found nearby";
+    }
+  }
   res.render('locations-list', {
     title: 'Loc8r - find a place to work with wifi',
     pageHeader: {
@@ -7,76 +23,156 @@ module.exports.homeList = function(req, res, next) {
       strapline: 'Find a place to work with wifi near you!'
     },
     sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake, or a pint? Let Loc8r help you find the place you're looking for.",
-    locations: [{
-      name: 'Starcups',
-      address: '2433 Rossini Blvd, Windsor ON, N8W 4P9',
-      rating: 3,
-      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-      distance: '100m'
-    },{
-      name: 'Starcups',
-      address: '2433 Rossini Blvd, Windsor ON, N8W 4P9',
-      rating: 4,
-      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-      distance: '200m'
-    },{
-      name: 'Starcups',
-      address: '2433 Rossini Blvd, Windsor ON, N8W 4P9',
-      rating: 5,
-      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-      distance: '300m'
-    }]
+    locations: responseBody,
+    message: message
   });
+}
+
+/* GET home page. */
+module.exports.homeList = function(req, res, next) {
+  var requestOptions, path;
+  path = '/api/locations';
+  requestOptions = {
+    url : apiOptions.server + path,
+    method : "GET",
+    json : {},
+    qs : {
+      lng : -82.97165851,
+      lat : 42.3035082,
+      maxDistance : 20
+    }
+  };
+  request(
+    requestOptions,
+    function(err, response, body) {
+      var i, data;
+      data = body;
+      if (response.statusCode === 200 && data.length) {
+        for (i = 0; i < data.length; i++) {
+          data[i].distance = _formatDistance(data[i].distance);
+        }
+      }
+      renderHomepage(req, res, data);
+    }
+  );
 };
 
-/* GET location info page. */
-module.exports.locationInfo = function(req, res, next) {
+var _formatDistance = function(distance) {
+  var numDistance, unit;
+  if (distance > 1) {
+    numDistance = parseFloat(distance).toFixed(1);
+    unit = ' km';
+  } else {
+    numDistance = parseInt(distance * 1000, 10);
+    unit = ' m';
+  }
+  return numDistance + unit;
+}
+
+var renderDetailPage = function(req, res, locDetail) {
   res.render('location-info', {
-    title: 'Starcups',
-    pageHeader: {title: 'Starcups'},
+    title: locDetail.name,
+    pageHeader: {title: locDetail.name},
     sidebar: {
       context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
       callToAction: 'If you\'ve been and you like it - or your don\'t - please leave a review to help other people just like you.',
     },
-    location: {
-      name: 'Starcups',
-      address: '2433 Rossini Blvd, Windsor ON, N8W 4P9',
-      rating: 3,
-      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-      coords: {lat: 42.3035082, lng: -82.9716585},
-      openingTimes: [{
-        days: 'Monday - Friday',
-        opening: '7:00am',
-        closing: '7:00pm',
-        closed: false
-      },{
-        days: 'Saturday',
-        opening: '8:00am',
-        closing: '5:00pm',
-        closed: false
-      },{
-        days: 'Sunday',
-        closed: true
-      }],
-      reviews: [{
-        author: 'Ben Blake',
-        rating: 5,
-        timestamp: '22 November 2017',
-        reviewText: 'What a great place. I can\'t say enough good things about it.'
-      },{
-        author: 'Benjamin Blake',
-        rating: 3,
-        timestamp: '21 November 2017',
-        reviewText: 'It was okay. Coffee wasn\'t great but the wifi was fast.'
-      }]
+    location: locDetail
+  });
+}
+
+var getLocationInfo = function(req, res, callback) {
+  var requestOptions, path;
+  path = "/api/locations/" + req.params.locationid;
+  requestOptions = {
+    url : apiOptions.server + path,
+    method : "GET",
+    json : {}
+  };
+  request(
+    requestOptions,
+    function(err, response, body) {
+      var data = body;
+      if (response.statusCode === 200) {
+        data.coords = {
+          lng : body.coords[0],
+          lat : body.coords[1]
+        };
+        callback(req, res, data);
+      } else {
+        _showError(req, res, response.statusCode);
+      }
     }
+  );
+}
+
+/* GET location info page. */
+module.exports.locationInfo = function(req, res, next) {
+  getLocationInfo(req, res, function(req, res, responseData) {
+    renderDetailPage(req, res, responseData);
   });
 };
 
+var _showError = function(req, res, status) {
+  var title, content;
+  if (status === 404) {
+    title = "404, page not found";
+    content = "Oh dear. Looks like we can't find this page. Sorry.";
+  } else {
+    title = status + ", something has gone wrong";
+    content = "Something, somewhere, has gone wrong.";
+  }
+  res.status(status);
+  res.render('generic-text', {
+    title : title,
+    content : content
+  });
+}
+
+var renderReviewForm = function(req, res, locDetail) {
+  res.render('location-review-form', {
+    title: 'Review ' + locDetail.name + ' on Loc8r',
+    pageHeader: {title: 'Review ' + locDetail.name},
+    error: req.query.err
+  });
+}
+
 /* GET add review info page. */
 module.exports.addReview = function(req, res, next) {
-  res.render('location-review-form', {
-    title: 'Review Starcups on Loc8r',
-    pageHeader: {title: 'Review Starcups'}
+  getLocationInfo(req, res, function(req, res, responseData) {
+    renderReviewForm(req, res, responseData);
   });
 };
+
+module.exports.doAddReview = function(req, res, next) {
+  var requestOptions, path, locationid, postData;
+  locationid = req.params.locationid;
+  path = "/api/locations/" + locationid + "/reviews";
+  postData = {
+    author: req.body.name,
+    rating: parseInt(req.body.rating, 10),
+    reviewText: req.body.review
+  };
+  requestOptions = {
+    url: apiOptions.server + path,
+    method: "POST",
+    json: postData
+  };
+  if (!postData.author || !postData.rating || !postData.reviewText) {
+    res.redirect('/location/' + locationid + '/review/new?err=val');
+  } else {
+    request(
+      requestOptions,
+      function(err, response, body) {
+        if (response.statusCode === 201) {
+          res.redirect('/location/' + locationid);
+        } else if (response.statusCode === 400 && body.name && body.name ===
+            "ValidationError") {
+          res.redirect('/location/' + locationid + '/review/new?err=val');
+        } else {
+          _showError(req, res, response.statusCode);
+        }
+      }
+    );
+  }
+}
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,8 +5,10 @@ var ctrlOthers = require('../controllers/others');
 
 /* Locations pages */
 router.get('/', ctrlLocations.homeList);
-router.get('/location', ctrlLocations.locationInfo);
-router.get('/location/review/new', ctrlLocations.addReview);
+router.get('/location/:locationid', ctrlLocations.locationInfo);
+router.get('/location/:locationid/review/new', ctrlLocations.addReview);
+router.post('/location/:locationid/review/new', ctrlLocations.doAddReview);
+
 
 /* Other pages */
 router.get('/about', ctrlOthers.about);
